f1584758afb407d5150ba0ed1f48f5608702f90a
Cocoflosbach <84343628+Cocoflosbach@users.noreply.github.com>
2022-02-08 23:33:06 +0100

    Update App.js to use NavigationContainer

--- App.js
+++ App.js
@@ -1,22 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from "react";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import "react-native-gesture-handler";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
 import StartScreen from "./components/StartScreen";
 import ChatScreen from "./components/ChatScreen";
+
+//import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
+//create the navigator
+const Stack = createStackNavigator();
+// const Tab = createBottomTabNavigator();
+
+export default class App extends Component {
+  render() {
+    return (
+      // Replace Stack with Tab for tab navigation view
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="StartScreen">
+          <Stack.Screen name="StartScreen" component={StartScreen} />
+
+          <Stack.Screen name="ChatScreen" component={ChatScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
