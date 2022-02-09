1c90d98cb02348b7a4fbb83653e2317cb621ffa9
Cocoflosbach <84343628+Cocoflosbach@users.noreply.github.com>
2022-02-09 08:24:20 +0100

    Update ChatScreen UI using giftedChat

--- components/ChatScreen.js
+++ components/ChatScreen.js
@@ -1,7 +1,38 @@
 import React, { Component } from "react";
 import { View, Text, Button, StyleSheet } from "react-native";
+import { GiftedChat } from "react-native-gifted-chat";
 
 export default class ChatScreen extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      messages: [],
+    };
+  }
+
+  componentDidMount() {
+    this.setState({
+      messages: [
+        {
+          _id: 1,
+          text: "Hello developer",
+          createdAt: new Date(),
+          user: {
+            _id: 2,
+            name: "React Native",
+            avatar: "https://placeimg.com/140/140/any",
+          },
+        },
+      ],
+    });
+  }
+
+  onSend(messages = []) {
+    this.setState((previousState) => ({
+      messages: GiftedChat.append(previousState.messages, messages),
+    }));
+  }
+
   render() {
     // name was passed  as prop from startscreen using navigate
     let name = this.props.route.params.name;
@@ -11,16 +42,13 @@ export default class ChatScreen extends Component {
     const { bgColor } = this.props.route.params;
 
     return (
-      <View
-        style={{
-          flex: 1,
-          justifyContent: "center",
-          alignItems: "center",
-          backgroundColor: bgColor,
+      <GiftedChat
+        messages={this.state.messages}
+        onSend={(messages) => this.onSend(messages)}
+        user={{
+          _id: 1,
         }}
-      >
-        <Text style={styles.text}>Welcome to the chat room!</Text>
-      </View>
+      />
     );
   }
 }
